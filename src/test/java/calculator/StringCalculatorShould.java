8e5b34a46bd2b671f46076fed083367393d3c413
Kaustubh-G <88483954+Kaustubh-G@users.noreply.github.com>
2021-10-17 19:00:40 +0530

    Created Init method
    Init method is declared and inside init method an object of StringCalculator is created.
    Method is annotated with @BeforeEach so we can get a new object of StringCalculator for every test method

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -30,52 +30,5 @@ class StringCalculatorShould {
     }
     
     
-    @Test
-    public void string_with_two_numbers_comma_delimited_should_return_sum()
-    {
-        assertEquals(10+20, stringCalculator.add("10,20"));
-    }
     
-    @Test
-    public void String_with_unknown_amout_of_numbers()
-    {
-    	assertEquals(3+6+15+18+46+33, stringCalculator.add("3,6,15,18,46,33"));
-    }
-    
-    @Test
-    public void string_with_new_lines_between_numbers()
-    {
-        assertEquals(3, stringCalculator.add("1\n2"));
-    }
-    
-    @Test
-    public void string_with_different_delimiters()
-    {
-    	assertEquals(3+6+15, stringCalculator.add("//;\n3;6;15") );
-    }
-    
-    @Test
-    public void string_with_single_negative_number_should_throw_an_exception()
-    {
-    	assertThrows(RuntimeException.class, ()->
-    		
-    		stringCalculator.add("3,-6,9,4")
-    	);
-    }
-    
-    @Test
-    public void string_with_multiple_negative_number_should_throw_an_exception()
-    {
-    		RuntimeException rex= null;
-    		try{
-    			stringCalculator.add("3,6,-9,-4,15");
-    		}catch(RuntimeException e)
-    		{
-    			rex=e;
-    		}
-    	
-    	assertNotNull(rex);
-    	assertEquals("negatives not allowed: [-9, -4]", rex.getMessage());
-    	
-    }
 }
